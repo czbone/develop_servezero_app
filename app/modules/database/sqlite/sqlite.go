62e4c100364c8fd374b0eec9c7fc74b4c3761bf0
naoki hirata <naoki@magic3.org>
2021-12-21 13:12:51 +0900

    DBアクセス処理更新。

--- app/modules/database/sqlite/sqlite.go
+++ app/modules/database/sqlite/sqlite.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql"
 	"os"
-	"strconv"
 	"web/config"
 	"web/modules/log"
 
@@ -91,58 +90,16 @@ func (db *BaseDb) QueryRows(query string, args ...interface{}) []map[string]inte
 }
 func setColVarType(colVar *[]interface{}, i int, typeName string) {
 	switch typeName {
-	case "INT":
+	case "INTEGER":
 		var s sql.NullInt64
 		(*colVar)[i] = &s
-	case "TINYINT":
-		var s sql.NullInt64
-		(*colVar)[i] = &s
-	case "MEDIUMINT":
-		var s sql.NullInt64
-		(*colVar)[i] = &s
-	case "SMALLINT":
-		var s sql.NullInt64
-		(*colVar)[i] = &s
-	case "BIGINT":
-		var s sql.NullInt64
-		(*colVar)[i] = &s
-	case "FLOAT":
+	case "REAL":
 		var s sql.NullFloat64
-		(*colVar)[i] = &s
-	case "DOUBLE":
-		var s sql.NullFloat64
-		(*colVar)[i] = &s
-	case "DECIMAL":
-		var s []uint8
-		(*colVar)[i] = &s
-	case "DATE":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "TIME":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "YEAR":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "DATETIME":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "TIMESTAMP":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "VARCHAR":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "MEDIUMTEXT":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "LONGTEXT":
-		var s sql.NullString
-		(*colVar)[i] = &s
-	case "TINYTEXT":
-		var s sql.NullString
 		(*colVar)[i] = &s
 	case "TEXT":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "BLOB":
 		var s sql.NullString
 		(*colVar)[i] = &s
 	default:
@@ -152,125 +109,28 @@ func setColVarType(colVar *[]interface{}, i int, typeName string) {
 }
 func setResultValue(result *map[string]interface{}, index string, colVar interface{}, typeName string) {
 	switch typeName {
-	case "INT":
+	case "INTEGER":
 		temp := *(colVar.(*sql.NullInt64))
 		if temp.Valid {
 			(*result)[index] = temp.Int64
 		} else {
 			(*result)[index] = nil
 		}
-	case "TINYINT":
-		temp := *(colVar.(*sql.NullInt64))
-		if temp.Valid {
-			(*result)[index] = temp.Int64
-		} else {
-			(*result)[index] = nil
-		}
-	case "MEDIUMINT":
-		temp := *(colVar.(*sql.NullInt64))
-		if temp.Valid {
-			(*result)[index] = temp.Int64
-		} else {
-			(*result)[index] = nil
-		}
-	case "SMALLINT":
-		temp := *(colVar.(*sql.NullInt64))
-		if temp.Valid {
-			(*result)[index] = temp.Int64
-		} else {
-			(*result)[index] = nil
-		}
-	case "BIGINT":
-		temp := *(colVar.(*sql.NullInt64))
-		if temp.Valid {
-			(*result)[index] = temp.Int64
-		} else {
-			(*result)[index] = nil
-		}
-	case "FLOAT":
+	case "REAL":
 		temp := *(colVar.(*sql.NullFloat64))
 		if temp.Valid {
 			(*result)[index] = temp.Float64
-		} else {
-			(*result)[index] = nil
-		}
-	case "DOUBLE":
-		temp := *(colVar.(*sql.NullFloat64))
-		if temp.Valid {
-			(*result)[index] = temp.Float64
-		} else {
-			(*result)[index] = nil
-		}
-	case "DECIMAL":
-		if len(*(colVar.(*[]uint8))) < 1 {
-			(*result)[index] = nil
-		} else {
-			(*result)[index], _ = strconv.ParseFloat(string(*(colVar.(*[]uint8))), 64)
-		}
-	case "DATE":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "TIME":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "YEAR":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "DATETIME":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "TIMESTAMP":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "VARCHAR":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "MEDIUMTEXT":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "LONGTEXT":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
-		} else {
-			(*result)[index] = nil
-		}
-	case "TINYTEXT":
-		temp := *(colVar.(*sql.NullString))
-		if temp.Valid {
-			(*result)[index] = temp.String
 		} else {
 			(*result)[index] = nil
 		}
 	case "TEXT":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "BLOB":
 		temp := *(colVar.(*sql.NullString))
 		if temp.Valid {
 			(*result)[index] = temp.String
