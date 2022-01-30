01187bc34d133fcf5e9d7b84a11a18002f43ad70
naoki hirata <naoki@magic3.org>
2022-01-30 18:10:10 +0900

    Nginxのuidを101に変更。

--- app/config/env.go
+++ app/config/env.go
@@ -26,7 +26,7 @@ var env = Env{
 	TemplatePath: "templates",
 
 	// Nginx設定ファイル
-	NginxUid:                 100,                                                  // Nginxプロセス実行ユーザ
+	NginxUid:                 101,                                                  // Nginxプロセス実行ユーザ
 	NginxSiteConfPath:        "/usr/local/servezero/volumes/nginx/sites-available", // サイト定義ファイル格納用
 	NginxSiteConfTemplateDir: "conf-templates",
 	NginxVirtualHostHome:     "/usr/local/servezero/volumes/vhost", // Webサイトホームディレクトリ
